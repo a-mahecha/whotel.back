af4e0b3b7da172f400117d6e74ef0887e2855217
Arlington Vargas <arlington.vargas4@gmail.com>
2024-04-23 22:59:42 -0500

    Change to mongo

--- routes/UserRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const UserController = require('../controllers/UserController');
-const checkAuth = require('../middleware/check-auth');
-
-router.post("/login", UserController.user_login);
-
-module.exports = router;
